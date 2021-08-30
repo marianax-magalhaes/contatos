e64902aa76fcdf9abed02268402532c6051d030c
marianax-magalhaes <marianaximenes@poli.ufrj.br>
2021-08-30 14:29:09 -0300

    contatos com loop

--- src/app/components/contato/contato.component.ts
+++ src/app/components/contato/contato.component.ts
@@ -1,4 +1,6 @@
+import { Input } from '@angular/core';
 import { Component, OnInit } from '@angular/core';
+import { Contato } from 'src/app/models/Contato';
 
 @Component({
   selector: 'app-contato',
@@ -6,6 +8,10 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./contato.component.css']
 })
 export class ContatoComponent implements OnInit {
+
+  // @inupt para indicar que obteremos os dados atraves de um input, de fora da classe.
+  // ! indica que ele será inicilizado foram desse ts
+  @Input("rc") registro!:Contato;
 
   constructor() { }
 
--- src/app/components/lista-de-contatos/lista-de-contatos.component.ts
+++ src/app/components/lista-de-contatos/lista-de-contatos.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Contato } from 'src/app/models/Contato';
 
 @Component({
   selector: 'app-lista-de-contatos',
@@ -6,6 +7,51 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./lista-de-contatos.component.css']
 })
 export class ListaDeContatosComponent implements OnInit {
+
+  //atributo contatos vai vir do model contato.ts 
+  //necessario esse [] para avissar que é um array!
+  contatos:Contato[] = [
+      {
+      nome:"Mariana Ribeiro 1",
+      email: "mariana@teste.com",
+      telefones:["111-111"]
+      },
+      {
+      nome:"Mariana Ribeiro 2",
+      email: "mariana@teste.com",
+      telefones:["222-222"]
+      },
+      {
+      nome:"Mariana Ribeiro 3",
+      email: "mariana@teste.com",
+      telefones:["333-333"]
+      },
+      {
+      nome:"Mariana Ribeiro 4",
+      email: "mariana@teste.com",
+      telefones:["444-444"]
+      },
+      {
+      nome:"Mariana Ribeiro 5",
+      email: "mariana@teste.com",
+      telefones:["555-555"]
+      },
+      {
+      nome:"Mariana Ribeiro 6",
+      email: "mariana@teste.com",
+      telefones:["666-666"]
+      },
+      {
+      nome:"Mariana Ribeiro 7",
+      email: "mariana@teste.com",
+      telefones:["777-777"]
+      },
+      {
+      nome:"Mariana Ribeiro 8",
+      email: "mariana@teste.com",
+      telefones:["888-888"]
+      }
+  ]
 
   constructor() { }
 
